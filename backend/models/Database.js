cf320bf608ddd04450c31e60be70ab7acd97c8ee
SANTOSH KUMAR SINGH <89244724+SANTO-C96@users.noreply.github.com>
2024-04-16 14:59:55 +0530

    Update Database.js
    update the mongodb database url

--- backend/models/Database.js
+++ backend/models/Database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connect(){
-    mongoose.connect('mongodb://localhost:27017/HRM').then(
+    mongoose.connect('mongodb+srv://santosh:Qp925uuMPRUFM7fV@cluster0.9mvwh8s.mongodb.net/HRM?retryWrites=true&w=majority&appName=Cluster0').then(
         ()=>{
             console.log("Mongodb Connected");
         }
